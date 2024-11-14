5d15a53053b6e41599e74e65c5a09e6e0021b3bd
Christopher Nielson <61611198+chrisjnielson44@users.noreply.github.com>
2024-11-14 13:48:40 -0500

    Doing small schema changes

--- recipe-app-frontend/recipe-app/src/app/dashboard/page.tsx
+++ recipe-app-frontend/recipe-app/src/app/dashboard/page.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import { Metadata } from 'next';
-import { Nav } from '@/app/dashboard/components/nav/Nav';
-import { UserNav } from '@/app/dashboard/components/nav/ProfileAvatar';
+import React from "react";
+import { Metadata } from "next";
+import { Nav } from "@/app/dashboard/components/nav/Nav";
+import { UserNav } from "@/app/dashboard/components/nav/ProfileAvatar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  PlusCircle,
+  Calendar,
+  ShoppingCart,
+  Apple,
+  Utensils,
+  Heart,
+} from "lucide-react";
 
 export const metadata: Metadata = {
-    title: 'Dashboard',
-    description: 'Dashboard',
+  title: "Dashboard",
+  description: "Dashboard",
 };
 
 export default async function AnalyticsPage() {
-    return (
-        <>
-            <div>
-                <Nav desktopProfile={<UserNav/>} mobileNav={<UserNav/>} />
-            </div>
-            <div className="flex-col md:flex">
-                <div className="flex-1 space-y-4 p-8 pt-4">
-                    <div className="flex items-center justify-between space-y-2">
-                        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <Nav desktopProfile={<UserNav />} mobileNav={<UserNav />} />
+      </div>
+      <div className="flex-col md:flex">
+        <div className="flex-1 space-y-4 p-8 pt-4">
+          <div className="flex items-center justify-between space-y-2">
+            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
